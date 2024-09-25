adee20091bca1161c588ee21e3344a59731ad22f
NathanBC21 <nathan.cabral@aluno.famper.edu.br>
2024-09-25 20:06:36 -0300

    Initial commit

---
+++ src/test/java/br/edu/famper/apicontroleestoque/ApiControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.apicontroleestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
